3c591acf7d482bf50da2d3695dbc3e6e6995556e
Edward Smith <edsmith6301@gmail.com>
2015-04-30 22:20:47 -0700

    added secret

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/secret', function(request, response) {
+  response.status(200).send('<h1>Hello, you\'ve stumbled across</h1>');
+});
 
 app.listen(process.env.PORT || 5000, function() {
   console.log('it running');
